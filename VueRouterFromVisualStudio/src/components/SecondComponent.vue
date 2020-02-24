cec4fc7c4c04e8bafe299d9bb39fcdc8f30d50c6
Julian Petersen <juli11c0@edu.ucl.dk>
2020-02-24 13:31:52 +0100

    navigate from inside the code

--- VueRouterFromVisualStudio/src/components/SecondComponent.vue
+++ VueRouterFromVisualStudio/src/components/SecondComponent.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>2. Route!!!</h1>
         <p>and a bit more text here.... this is the data from the variable in the link. teh passed data is: {{$route.params.id}}</p>
+        <button @click="goHome">back to home</button>
     </div>
 </template>
 
@@ -11,13 +12,20 @@
         //vue manages teh components eficently, therefor things in created will not always run, because it doesnt get created everytime
         //for this we make a watch proptery
         //we are watching the route and now the methods run everytime
-        watch: {
-            '$route'(to, from) {
-                alert(to.params.id);
+        //watch: {
+        //    '$route'(to, from) {
+        //        alert(to.params.id);
+        //    }
+        //},
+        //created() {
+        //    alert(this.$route.params.id);
+        //},
+        methods: {
+            goHome() {
+                //we push a new route to the stack
+                //navigate from inside the code
+                this.$router.push('/');
             }
-        },
-        created() {
-            alert(this.$route.params.id);
         }
     }
 </script>
